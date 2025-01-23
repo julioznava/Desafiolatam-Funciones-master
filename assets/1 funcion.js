ad8112f5a4912f6141e8da21240fc0659826b3d9
julioznava <junavarro.rojas@gmail.com>
2025-01-23 12:12:12 -0300

    arreglo final

--- assets/1 funcion.js
+++ assets/1 funcion.js
@@ -7,7 +7,7 @@ function example(a, b, c){
 
 /*Transforma esta declaración de función a una función de expresión*/
 example = function (a, b, c) {
-    return a + b + ;
+    return a + b + c;
 };
 
 /*Pruebas*/
